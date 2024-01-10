5f41c181893721698ddd1d42943bcb19fd9124fa
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-10 14:46:30 +0200

    Design website home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return Inertia::render('home');
+        return Inertia::render('Home');
     }
 }
