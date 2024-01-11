876484de5c944fd30a660abc4d4a9ab55db3ad46
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-11 20:53:33 +0200

    Implement creating posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+
+
+    public function store(StorePostRequest $request)
+    {
+        $data = $request->validated();
+
+        Post::create($data);
+
+        return back();
+    }
+
+
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        //
+    }
+
+
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -1,5 +1,8 @@
 <script setup>
 import PostItem from "@/Components/app/PostItem.vue";
+defineProps({
+    posts: Array
+})
 const post1 = {
     user: {
         id: 1,
@@ -53,8 +56,7 @@ const post2 = {
 
 <template>
     <div class="overflow-auto">
-        <PostItem :post="post1"/>
-        <PostItem :post="post2"/>
+        <PostItem v-for="post of posts" :key="post.id" :post="post"/>
     </div>
 </template>
 
