b6e76fabd5263386e54918d8d4bd3d7505171cca
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-20 03:17:07 +0200

    Implement downloading post attachments

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,30 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('dashboard');
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 Route::get('/u/{user:username}', [ProfileController::class, 'index'])
     ->name('profile');
 
-Route::middleware('auth')->group(function () {
-    Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
-        ->name('profile.updateImages');
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')
+    ->group(function () {
+        Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
+            ->name('profile.updateImages');
+    //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])
+            ->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])
+            ->name('profile.destroy');
 
-    Route::post('/posts', [PostController::class, 'store'])->name('post.create');
-    Route::put('/posts/{post}', [PostController::class, 'update'])->name('post.update');
-    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+        Route::post('/posts', [PostController::class, 'store'])
+            ->name('post.create');
+        Route::put('/posts/{post}', [PostController::class, 'update'])
+            ->name('post.update');
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+            ->name('post.destroy');
+        Route::get('/post/download/{attachment}', [PostController::class, 'downloadAttachment'])
+            ->name('post.download');
+
 });
 
 require __DIR__.'/auth.php';
