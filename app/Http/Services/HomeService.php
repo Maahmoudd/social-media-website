dfb96bdf80f121d9394046bc08e21b2d422ed93c
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-27 02:03:54 +0200

    Fetching all post comments in single query

--- app/Http/Services/HomeService.php
+++ app/Http/Services/HomeService.php
@@ -10,24 +10,16 @@ class HomeService
     public function index()
     {
         $userId = Auth::id();
-        $posts = Post::query()
-            ->withCount('reactions')
-            ->withCount('comments')
-            ->with([
-                'comments' => function ($query) use ($userId) {
-                    $query
-                        ->whereNull('parent_id')
-                        ->withCount('reactions')
-                        ->withCount('comments')
-                        ->with([
-                            'reactions' => function ($query) use ($userId) {
-                                $query->where('user_id', $userId);
-                            }
-                        ]);
-                },
-                'reactions' => function ($query) use ($userId) {
-                    $query->where('user_id', $userId);
-                }])
+        $posts = Post::query() // SELECT * FROM posts
+        ->withCount('reactions') // SELECT COUNT(*) from reactions
+        ->with([
+            'comments' => function ($query) use ($userId) {
+                $query->withCount('reactions'); // SELECT * FROM comments WHERE post_id IN (1, 2, 3...)
+                // SELECT COUNT(*) from reactions
+            },
+            'reactions' => function ($query) use ($userId) {
+                $query->where('user_id', $userId); // SELECT * from reactions WHERE user_id = ?
+            }])
             ->latest()
             ->paginate(20);
         return $posts;
